aeae89c2a51d41729bd3e5ad23ae0bcd283bc967
Dzhalal Atabaev <djaldoit@gmail.com>
2024-05-16 11:07:33 +0300

    Completed hw qa_quru13_2

--- test_task_1.py
+++ test_task_1.py
@@ -5,10 +5,10 @@ def test_dark_theme_by_time():
     """
     Протестируйте правильность переключения темной темы на сайте в зависимости от времени
     """
-    current_time = time(hour=23)
+    current_time = time(hour=4)
     # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
 
-    if current_time.hour > 22 or current_time.hour < 7:
+    if current_time.hour >= 22 or current_time.hour < 6:
         is_dark_theme = True
     else:
         is_dark_theme = False
@@ -32,7 +32,7 @@ def test_dark_theme_by_time_and_user_choice():
     if dark_theme_enabled_by_user:
         is_dark_theme = True
     elif dark_theme_enabled_by_user is None:
-        if current_time.hour > 22 or current_time.hour < 7:
+        if current_time.hour >= 22 or current_time.hour < 6:
             is_dark_theme = True
         else:
             is_dark_theme = False
